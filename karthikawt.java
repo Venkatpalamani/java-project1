4b333a913ff40f85f08648d8e5277dbf6c43cc07
Venkatpalamani <venkatkarthik1812@gmail.com>
2025-09-26 01:22:00 +0530

    Add files via upload

--- karthikawt.java
+++ karthikawt.java
@@ -3,21 +3,22 @@ import java.awt.*;
 import java.awt.event.*;
 public class karthikawt extends Frame {
 	karthikawt(){
-	Label l = new Label("welcome");
-	Button b = new Button("ok");
-	setLayout(new FlowLayout());
-	
-	add(l);
-	add(b);
-	
-	setSize(300,300);
-	setVisible(true);
-	
-	addWindowListener(new WindowAdapter() {
-		public void windowClosing(WindowEvent e) {
-			System.exit(0);
-		}
-	});
+		Label lb = new Label("click on Download to continue");
+		Button bt = new Button("DOWNLOAD");
+		
+		setLayout(new FlowLayout());
+		
+		add(lb);
+		add(bt);
+		
+		setSize(300,300);
+		setVisible(true);
+		
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
 	}
 	public static void main(String[] args) {
 		karthikawt obj = new karthikawt();
